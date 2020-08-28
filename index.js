9f6a1baad9ec595f3b672734da963958585e634e
Warren Kavanagh <WarrenKavanagh1000@gmail.com>
2020-08-28 15:05:14 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// *** Modules ***
+//  express - Framework for the app 
+//
+const express = require('express');
+
+// Define an instance of express 
+const app = express();
+
+const port = process.env.PORT;
+console.log(`Your port is ${port}`);
