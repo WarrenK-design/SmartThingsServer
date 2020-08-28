5b7f178c771e140db045eabac9ea026e69be676a
Warren Kavanagh <WarrenKavanagh1000@gmail.com>
2020-08-28 17:09:27 +0100

    Second commit, built a logger function

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 // *** Modules ***
 //  express - Framework for the app 
-//
+//  dotenv  - Used to read the env file
+//  logger  - Used to log incoming http messages to console  
 const express = require('express');
+const dotenv  = require('dotenv');
+const logger  = require('./middleware/logger')
+
 
 // Define an instance of express 
 const app = express();
 
-const port = process.env.PORT;
-console.log(`Your port is ${port}`);
+// Middleware 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(logger);
+
+
+// Read the env file 
+dotenv.config();
+
+// Route the first page 
+app.get('/',(req,res) => {
+    res.send("Hello")
+})
+
+// Let the server run 
+app.listen(process.env.PORT)
---
+++ middleware/logger.js
@@ -0,0 +1,21 @@
+// Used to log incoming requests to the console 
+
+// Modules 
+//  moment - Formats the dates to log out to the console 
+const moment = require('moment');
+
+// Setup the logger 
+const logger = (req,res,next) =>{
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl} ${moment().format()}`);
+    console.log(`${req.method}`)
+    if(req.method == 'POST'||req.method =='PUT'){
+        console.log(`Body of ${req.method}\n${req.body.params}`)
+        next();
+    }
+    else{
+        next();
+    }
+};
+
+// Export the module 
+module.exports = logger;
